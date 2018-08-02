6587b6f6c4d9e6b419ab247e8f0ea05dec11e785
Zoran Rajic <zox@portworx.com>
2018-08-01 21:22:37 -0700

    Fixing PX-deployment scripts

--- scripts/50-install_portworx_oci_daemonset.sh
+++ scripts/50-install_portworx_oci_daemonset.sh
@@ -5,7 +5,7 @@ hostname -I | grep -wq $K8S_MASTER_IP
 if [ $? -eq 0 ]; then
     echo ':: Installing Portworx OCI DaemonSet ...'
     export KUBECONFIG=/etc/kubernetes/admin.conf
-    kubectl apply -f "http://install.portworx.com:8080?type=oci&c=mycluster22&k=etcd://${K8S_MASTER_IP}:2379&d=${MYVMIF}&m=${MYVMIF}&s=${MYSTORAGE}"
+    kubectl apply -f "https://install.portworx.com?c=mycluster22&k=etcd://${K8S_MASTER_IP}:2379&d=${MYVMIF}&m=${MYVMIF}&s=${MYSTORAGE}"
 
 else
     echo '(skipping install of Portworx on this node)'
