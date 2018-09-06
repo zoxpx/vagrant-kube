225f79a32e67a1aa51b715b6f0a2e9e20eff6565
Zoran Rajic <zox@portworx.com>
2018-09-06 15:43:55 -0700

    Fixing _VARIETY.sh

--- _VARIETY.sh
+++ _VARIETY.sh
@@ -2,14 +2,15 @@
 
 echo kb1 kb2 kb3 kb4 kb5 | xargs -n1 salt-key -yd
 
-export KUBE_OSTYPE=ubuntu16
+#export KUBE_OSTYPE=ubuntu16
+export KUBE_OSTYPE=bento16
 
 # KB1 setup
 
 chmod 755 scripts/10a-install_docker_latest.sh
 chmod 644 scripts/10b-install_docker_native.sh
 chmod 755 scripts/30a-install_kubernetes_latest.sh
-chmod 644 scripts/30b-install_kubernetes-v1.7.sh
+chmod 644 scripts/30b-install_kubernetes-v1.8.sh
 
 vagrant up kb1
 
